bca6af35065a8dc9dc315f4040040ad885ec2fcb
tacosz <nabilnasruddin.offc@gmail.com>
2024-02-27 18:50:14 +0700

    Add project files.

---
+++ PrakPertamaPD/PrakPertamaPD.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    //numeric nJejari, nLuas
+    //display 'masukan jejari= '
+    //accept nJejari
+    //compute nLuas = 3.14 * r * r
+    //display 'Luasnya= ' + nLuas
+
+    float nJejari, nLuas;
+    cout << " Masukan jejari = ";
+    cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << " Luasnya = " << nLuas << endl;
+
+    //numeric nPanjang, nLebar
+    //display 'masukan panjang= '
+    //accept nPanjang
+    //display 'masukan lebar= '
+    //accept nLebar
+    //compute nLuas = nPanjang * nLebar
+    //display 'Luasnya= ' + nLuas
+
+    int nPanjang, nLebar;
+    cout << "Masukan Panjang= ";
+    cin >> nPanjang;
+    cout << "Masukan Lebar= ";
+    cin >> nLebar;
+    nLuas = nPanjang * nLebar;
+    cout << "Luasnya = " << nLuas << endl;
+
+}//end
